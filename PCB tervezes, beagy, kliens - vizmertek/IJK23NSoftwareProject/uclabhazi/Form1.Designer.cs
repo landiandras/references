317a86d5ce9bca4d269a95bb7d88cb16e84ef9c7
landiandras <126565717+landiandras@users.noreply.github.com>
2025-01-19 18:37:54 +0100

    Add files via upload

---
+++ PCB tervezes, beagy, kliens - vizmertek/IJK23NSoftwareProject/uclabhazi/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace uclabhazi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TextProgressBar1 = new uclabhazi.TextProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.ReadBufferSize = 2;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 249);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 201);
+            this.panel2.TabIndex = 6;
+            this.panel2.SizeChanged += new System.EventHandler(this.panel2_SizeChanged);
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // TextProgressBar1
+            // 
+            this.TextProgressBar1.CustomText = "";
+            this.TextProgressBar1.Location = new System.Drawing.Point(319, 81);
+            this.TextProgressBar1.Name = "TextProgressBar1";
+            this.TextProgressBar1.ProgressColor = System.Drawing.Color.LightGreen;
+            this.TextProgressBar1.Size = new System.Drawing.Size(185, 60);
+            this.TextProgressBar1.TabIndex = 0;
+            this.TextProgressBar1.TextColor = System.Drawing.Color.Black;
+            this.TextProgressBar1.TextFont = new System.Drawing.Font("Times New Roman", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.TextProgressBar1.VisualMode = uclabhazi.ProgressBarDisplayMode.CurrProgress;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(315, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 53);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox1_PreviewKeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(507, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(315, 43);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(186, 60);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(249, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 60);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 48);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Fényerő:";
+            this.label1.UseMnemonic = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private SerialPort serialPort1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Panel panel2;
+        private TextProgressBar TextProgressBar1;
+        private TextBox textBox1;
+        private Button button2;
+        private ProgressBar progressBar1;
+        private Button button1;
+        private Label label1;
+    }
+}
+
---
+++ PCB tervezes, beagy, kliens - vizmertek/IJK23NSoftwareProject/uclabhazi/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uclabhazi
+{
+    public partial class Form1 : Form
+    {      
+        private int tiltx = 0;
+        private int tilty = 0;
+        private bool setbrigthness = false;
+        private byte brightness = 0;
+        private bool textvalid = true;
+        private string prevtext;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval =300;
+            timer2.Interval = 50;
+            timer1.Start();
+            timer2.Start();
+
+            textBox1.Text = "50";
+
+            if(!serialPort1.IsOpen)serialPort1.Open();
+            serialPort1.DtrEnable = true;
+            serialPort1.DiscardInBuffer();
+
+ 
+
+
+        }
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine(serialPort1.ReadExisting());
+        }
+
+        private void portRequestData()
+        {
+            byte[] buffer = new byte[3];
+            buffer[0] = 0xAA;
+            buffer[1] = 0x01;
+            buffer[2] = 0x02;
+            serialPort1.Write(buffer, 0, 3);
+        }
+        private void portSetBrightness(byte n)
+        {
+            
+
+            byte[] buffer = new byte[4];
+            System.Threading.Thread.Sleep(1);
+            buffer[0] = 0xAA;
+            buffer[1] = 0x02;
+            buffer[2] = 0x01;
+            buffer[3] = n;
+            if(!serialPort1.IsOpen) serialPort1.Open();
+
+            serialPort1.Write(buffer, 0, 1);
+            System.Threading.Thread.Sleep(1);
+            serialPort1.Write(buffer, 1, 1);
+            System.Threading.Thread.Sleep(1);
+            serialPort1.Write(buffer, 2, 1);
+            System.Threading.Thread.Sleep(1);
+            serialPort1.Write(buffer, 3, 1);
+            System.Threading.Thread.Sleep(1);
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setbrigthness = true;
+            brightness = byte.Parse(textBox1.Text);
+            
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.DrawRectangle(System.Drawing.Pens.Blue, 10, ClientSize.Height / 2, ClientSize.Width-20, ClientSize.Height/2-10);
+            //Point center = new Point(ClientSize.Width/2, ClientSize.Height/2);
+            //Point bubble = new Point(center.X+(tiltx*(ClientSize.Width/2)) / 30, (center.Y + (tilty * (ClientSize.Height / 2) / 30)));
+            //e.Graphics.DrawEllipse(System.Drawing.Pens.Blue, bubble.X, bubble.Y, 40, 40);
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            if (serialPort1.BytesToRead!=0)
+            {
+                byte[] buffer = new byte[2];
+                serialPort1.Read(buffer, 0, 2);
+                if (buffer[0] != 0) tilty = buffer[0] - 90;
+                if (buffer[1] != 0) tiltx = buffer[1] - 90;
+            }
+            panel2.Invalidate();
+   
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //serialPort1.Open();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textvalid)
+            {
+                if (textBox1.Text.Length > 0 && textBox1.Text.Length < 3)
+                {
+                    brightness = byte.Parse(textBox1.Text);
+                    portSetBrightness(byte.Parse(textBox1.Text));
+                }
+                else if (textBox1.Text.Length >= 3)
+                {
+                    brightness = 100;
+                    portSetBrightness(100);
+                    textBox1.Text = "100";
+                }
+                else
+                {
+                    portSetBrightness(0);
+                    brightness = 0;
+                }
+                prevtext = textBox1.Text;
+            }
+            else textBox1.Text = prevtext;
+            progressBar1.Value = brightness;
+        }
+
+        private void textBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if ((e.KeyValue < '0' || e.KeyValue > '9') && e.KeyValue!= 8 && e.KeyValue != 46)
+            {
+                textvalid = false;
+            }
+            else textvalid = true;
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(System.Drawing.Pens.Blue, 10, panel2.Height/2, panel2.Width-10, panel2.Height/2);
+            e.Graphics.DrawLine(System.Drawing.Pens.Blue, panel2.Width / 2, panel2.Height - 10, panel2.Width / 2, 10);
+            e.Graphics.DrawRectangle(System.Drawing.Pens.Blue, 10, 10, panel2.Width - 20, panel2.Height - 20);
+            Point center = new Point(panel2.Width / 2, panel2.Height / 2);
+            Point bubble = new Point(center.X + (tiltx * (panel2.Width / 2)) / 30, (center.Y + (tilty * (panel2.Height / 2) / 30)));
+            if(bubble.X > panel2.Width-30) bubble.X = panel2.Width-30;
+            if(bubble.X < 30) bubble.X = 30;
+            if(bubble.Y <30 ) bubble.Y = 30;
+            if(bubble.Y > panel2.Height-30) bubble.Y= panel2.Height-30;
+            e.Graphics.DrawEllipse(System.Drawing.Pens.Blue, bubble.X-20, bubble.Y-20, 40, 40);
+        }
+
+        private void panel2_SizeChanged(object sender, EventArgs e)
+        {
+            panel2.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (brightness > 90) brightness = 100;
+            else brightness = (byte)(brightness + 10);
+            textBox1.Text = brightness.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (brightness < 10) brightness = 0;
+            else brightness = (byte)(brightness - 10);
+            textBox1.Text= brightness.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
